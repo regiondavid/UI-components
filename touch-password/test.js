87b358f2ad2b55f4269f0893892800755781a200
regiondavid <uestczjw@gmail.com>
2017-03-24 10:36:49 +0800

    touch-password init

---
+++ touch-password/test.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  var test1 = document.getElementById("test1");
+  var test2 = document.getElementById("test2");
+  // test1.onmousedown = function(event) {
+  //   // console.log(1);
+  //   // console.log(event.clientX)
+
+  // }
+  // test1.onmouseup = function() {
+  //   console.log("up");
+  // }
+  test1.ontouchmove = function(event) {
+    console.log(event.clientX)
+    console.log(2);
+  }
+}
